e57779d5d33380a4d6e0d4c2a9329a7c017711f0
rzalism-ic <ismayil@intelligentcore.io>
2026-03-15 22:19:59 +0400

    main

--- resources/views/livewire/admin/applications/student/transfer-letter-english.blade.php
+++ resources/views/livewire/admin/applications/student/transfer-letter-english.blade.php
@@ -307,7 +307,7 @@
 
     <div class="verification-footer">
         <!-- Verification Box with QR Code -->
-        <table style="width: 100%; margin-bottom: 10px;">
+        <table style="width: 100%; margin-top: 40px;">
             <tr>
                 <!-- QR Code -->
 
