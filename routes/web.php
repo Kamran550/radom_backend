50373a80aaa72b1de17c1a78df0fc6200c37703e
rzalism-ic <ismayil@intelligentcore.io>
2026-04-29 17:31:23 +0400

    nain

--- routes/web.php
+++ routes/web.php
@@ -8,16 +8,16 @@ Route::domain('admin.radomuniversity.pl')->group(function () {
 });
 
 
-Route::domain('teacher.admin.radomuniversity.pl')->group(function () {
+Route::domain('teacher.radomuniversity.pl')->group(function () {
     require base_path('routes/teacher.php');
 });
 
 
-Route::domain('student.admin.radomuniversity.pl')->group(function () {
+Route::domain('student.radomuniversity.pl')->group(function () {
     require base_path('routes/student.php');
 });
 
 
-Route::domain('validate.admin.radomuniversity.pl')->group(function () {
+Route::domain('verify.radomuniversity.pl')->group(function () {
     require base_path(path: 'routes/verify.php');
 });
